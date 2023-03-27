44b4dc28fea9a0e9a724e93760276cb13a0358cc
Mukesh-pilane <mukeshpilane@gmail.com>
2023-03-28 00:10:03 +0530

    deploy to render

---
+++ api/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+
+//environment variables
+PORT = process.env.PORT || 5001;
+API_KEY = process.env.API_KEY;
+
+const app = express()
+
+//middleWares
+app.use(cors())
+
+//api routes
+app.get("/api/:term", async (req,res)=>{
+  
+  let json;
+  const term = req.params.term;
+  const url = `https://newsapi.org/v2/everything?q=${term}&apiKey=${API_KEY}`;
+  
+  try {
+        const response = await fetch(url);
+        json = await response.json();
+        console.log(response)
+      } catch (error) {
+        console.log("error", error);
+      }
+  res.send(json)
+})
+
+app.listen(PORT, (err)=>{
+  if(err){
+    console.log(err)
+  }else{
+    console.log(`Server is listening on port ${PORT}`)
+  }
+})
